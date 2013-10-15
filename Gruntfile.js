90308988c943a3dc7f2ef0d07e368054365c9e14
Anenth <anenthvishnu@gmail.com>
2013-10-15 10:11:07 +0530

    tunning

--- Gruntfile.js
+++ Gruntfile.js
@@ -257,6 +257,18 @@ module.exports = function (grunt) {
                         'styles/fonts/{,*/}*.*',
                         'bower_components/font-awesome/font/*.*',
                     ]
+                },{
+                    expand: true,
+                    dest: '<%= yeoman.dist %>',
+                    cwd: 'heroku',
+                    src: '*',
+                    rename: function (dest, src) {
+                        var path = require('path');
+                        if (src === 'distpackage.json') {
+                            return path.join(dest, 'package.json');
+                        }
+                        return path.join(dest, src);
+                    }
                 }]
             },
             styles: {
